93af38b748324ad6ea40c3f74958f68329730030
alexcoldbrew <alexcoldbrew@gmail.com>
2020-11-06 12:02:53 -0600

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    # attr_accessor :title, :content
+
+    # @@all = []
+
+    # def initialize
+    #     @title = title
+    #     @content = content
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+
+
 end
